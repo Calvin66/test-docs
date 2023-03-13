59a1000399ebc3ec562593757be20c20b19c61d6
李建国19 <627935@ky-tech.com.cn>
2023-03-13 20:18:48 +0800

    feat:菜单修改

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -1,6 +1,15 @@
 module.exports = [
   { text: "首页", link: "/" },
-  // { text: "文档", link: "/" },
-  // { text: "FAQ", link: "/" },
-  { text: "GitHub", link: "https://github.com/" },
+  {
+    text: "开发文档",
+    link: "/pages/010101/",
+    items: [
+      { text: "环境搭建部署", link: "/pages/010101/" },
+      { text: "快速入门", link: "/pages/010201/" },
+      { text: "特别鸣谢", link: "/pages/010301/" },
+    ]
+  },
+  { text: "FAQ", link: "https://github.com/KYE-Galaxy-BigData/chitu-sdp/issues" },
+  { text: "GitHub", link: "https://github.com/KYE-Galaxy-BigData/chitu-sdp" },
+
 ];
