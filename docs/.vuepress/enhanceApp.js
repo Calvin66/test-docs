a8f46c52653833eca01fbbcf64d7512fbdec35d3
李建国19 <627935@ky-tech.com.cn>
2023-03-14 11:51:51 +0800

    feat:修复引入组件打包报错问题

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,13 +1,18 @@
 import { Button } from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-import VueDPlayer from 'vue-dplayer'
+// import VueDPlayer from 'vue-dplayer'
 import 'vue-dplayer/dist/vue-dplayer.css'
-export default ({
+export default async ({
   Vue, // VuePress 正在使用的 Vue 构造函数
   options, // 附加到根实例的一些选项
   router, // 当前应用的路由实例
-  siteData // 站点元数据
+  siteData, // 站点元数据
+  isServer
 }) => {
   Vue.use(Button);
-  Vue.use(VueDPlayer);
+  if (!isServer) {
+    await import('vue-dplayer').then(module => {
+      Vue.use(module.default)
+    })
+  }
 }
