3441d5927dbbf055f348291600f832b1c4e11a1e
李建国19 <627935@ky-tech.com.cn>
2023-03-14 10:13:29 +0800

    feat:引入ele组件、视频播放器组件

--- docs/.vuepress/components/SpecialLayout.vue
+++ docs/.vuepress/components/SpecialLayout.vue
@@ -1,6 +1,20 @@
 <template>
-  <div class="container">
-    首页
+  <div class="layout-container">
+    <div class="platform-container flex">
+      <div class="left">
+        <h1>赤兔实时计算平台</h1>
+        <div>基于Apache Flink构建的企业级、一站式、高性能、低门槛实时大数据计算平台，广泛适用于流式数据开发应用场景。</div>
+      </div>
+      <div class="right">
+        <!-- <el-button type="success">按钮</el-button> -->
+        <d-player ref="player"
+                  :options="dplayerOpt"></d-player>
+      </div>
+    </div>
+    <div class="application-container"></div>
+    <div class="feature-container"></div>
+    <div class="function-container"></div>
+    <div class="footer-container"></div>
   </div>
 </template>
 
@@ -12,8 +26,12 @@
     props: {},
     data () {
       return {
-
-      };
+        dplayerOpt: {
+          video: {
+            url: "https://bg-prd-cos-bdp-1257092428.cos.ap-guangzhou.myqcloud.com/bigdata/common/app/2023/0/14/211673703901088.mp4",
+          },
+        },
+      }
     },
     computed: {},
     watch: {},
@@ -23,4 +41,56 @@
   }
 </script>
 <style lang='scss' scoped>
+  .layout-container {
+    .platform-container {
+      .left,
+      .right {
+        width: 50%;
+      }
+    }
+    .flex {
+      display: flex;
+    }
+
+    .f-jcfs {
+      justify-content: flex-start;
+    }
+
+    .f-jcfe {
+      justify-content: flex-end;
+    }
+
+    .f-jcc {
+      justify-content: center;
+    }
+
+    .f-jcsa {
+      justify-content: space-around;
+    }
+
+    .f-jcsb {
+      justify-content: space-between;
+    }
+
+    .f-aic {
+      align-items: center;
+    }
+
+    .f-aifs {
+      align-items: flex-start;
+    }
+
+    .f-aife {
+      align-items: flex-end;
+    }
+
+    .f-1 {
+      flex: 1;
+    }
+
+    .flex-d-col {
+      display: flex;
+      flex-direction: column;
+    }
+  }
 </style>
--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,8 +1,13 @@
-
+import { Button } from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import VueDPlayer from 'vue-dplayer'
+import 'vue-dplayer/dist/vue-dplayer.css'
 export default ({
   Vue, // VuePress 正在使用的 Vue 构造函数
   options, // 附加到根实例的一些选项
   router, // 当前应用的路由实例
   siteData // 站点元数据
 }) => {
+  Vue.use(Button);
+  Vue.use(VueDPlayer);
 }
